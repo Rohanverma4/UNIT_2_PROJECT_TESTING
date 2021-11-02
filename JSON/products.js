6b4c76363ac17c65643890cd53fefe10e6a7e508
Aman Kumar <isharaman8@gmail.com>
2021-11-02 12:53:13 +0530

    updated products.js, added more products

--- JSON/products.js
+++ JSON/products.js
@@ -1,6 +1,7 @@
 var products = [
 	{
 		name: "Novy Pain Oil",
+		img_src: "../IMAGES/PRODUCT_IMAGES/novy_oil.jpg",
 		price: 99,
 		features: [
 			"100% Freshly Handpicked Herbs",
@@ -19,6 +20,7 @@ var products = [
 	},
 	{
 		name: "Conarcs Diabetic Ointment",
+		img_src: "../IMAGES/PRODUCT_IMAGES/conarcs.jpg",
 		price: 140,
 		features: [
 			"100% Natural Remedy",
@@ -32,6 +34,45 @@ var products = [
 			"Product Description":
 				" Conarcs Diabetic Ointment is manufactured by Conarcs",
 			Packing: "Container",
+			Condition: "New",
+			"Sold by": "Pulse Pharmacy India Pvt. Ltd.",
+		},
+	},
+	{
+		name: "Shigaa Anti Lice & Dandruff Oil",
+		img_src: "../IMAGES/PRODUCT_IMAGES/shigaa.png",
+		price: 110,
+		features: [
+			"No need of compulsory bathing & combing",
+			"100% herbal, no side effects, no hairloss",
+			"Get rid of dandruff in 2 weeks",
+		],
+		available: true,
+		details: {
+			SKU: "PC-44072",
+			"Product Description":
+				"Shigaa Anti Lice & Dandruff Oil is manufactured by Dhevi Herbal Products",
+			Packing: "Unit",
+			Condition: "New",
+			"Sold by": "Pulse Pharmacy India Pvt. Ltd.",
+		},
+	},
+	{
+		name: "Pankajakasthuri Orthoherb Tablet 30'S",
+		img_src: "../IMAGES/PRODUCT_IMAGES/pankajakasthuri.jpg",
+		price: 145,
+		features: [
+			"Instant Joint Pain Relief",
+			"Herbal, free from chemicals",
+			"Relieves Back, Shoulder & Ankle Pain",
+			"Get rid of dandruff in 2 weeks",
+		],
+		available: true,
+		details: {
+			SKU: "PC-26998",
+			"Product Description":
+				"Pankajakasthuri Orthoherb Tablet 30'S is manufactured by Pankajakasthuri",
+			Packing: "Packet",
 			Condition: "New",
 			"Sold by": "Pulse Pharmacy India Pvt. Ltd.",
 		},
