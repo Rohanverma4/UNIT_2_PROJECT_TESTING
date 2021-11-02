ea6a993438c340c1487f5acbda586bdaca39fefa
Aman Kumar <isharaman8@gmail.com>
2021-11-02 12:40:15 +0530

    added products in json format

---
+++ JSON/products.js
@@ -0,0 +1,39 @@
+var products = [
+	{
+		name: "Novy Pain Oil",
+		price: 99,
+		features: [
+			"100% Freshly Handpicked Herbs",
+			"Relief for Arthritis, Knee/Back Pain",
+			"Headache, Cold, Sinus",
+			"Ayurveda | No Side Effects",
+		],
+		available: true,
+		details: {
+			SKU: "PC-41766",
+			"Product Description": "Novy Pain Oil is manufactured by Fugle Pharma",
+			Packing: "Bottle",
+			Condition: "New",
+			"Sold by": "Pulse Pharmacy India Pvt. Ltd.",
+		},
+	},
+	{
+		name: "Conarcs Diabetic Ointment",
+		price: 140,
+		features: [
+			"100% Natural Remedy",
+			"Reduces Fatigue",
+			"Reduces Sugar to Normal Levels",
+			"Prevents",
+		],
+		available: true,
+		details: {
+			SKU: "PC-41767",
+			"Product Description":
+				" Conarcs Diabetic Ointment is manufactured by Conarcs",
+			Packing: "Container",
+			Condition: "New",
+			"Sold by": "Pulse Pharmacy India Pvt. Ltd.",
+		},
+	},
+];
