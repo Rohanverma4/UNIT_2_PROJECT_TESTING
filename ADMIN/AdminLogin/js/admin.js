cd8e0f86f8f487e018410cf3c7cab1301442934f
I_am_rohan <88994067+Rohanverma4@users.noreply.github.com>
2021-11-04 19:00:56 +0530

    some part of security..

--- ADMIN/AdminLogin/js/admin.js
+++ ADMIN/AdminLogin/js/admin.js
@@ -1,30 +1,30 @@
-document.querySelector("form").addEventListener("submit", loginAdmin);
-let userDetails = JSON.parse(localStorage.getItem("Users")) || [];
-function loginAdmin(event) {
-	event.preventDefault();
-	let enteredUsername = document.getElementById("username").value;
-	let enteredPassword = document.getElementById("password").value;
-	// let checkedBox = document.getElementById("saveUserCreds");
+// document.querySelector("form").addEventListener("submit", loginAdmin);
+// let userDetails = JSON.parse(localStorage.getItem("Users")) || [];
+// function loginAdmin(event) {
+// 	event.preventDefault();
+// 	let enteredUsername = document.getElementById("username").value;
+// 	let enteredPassword = document.getElementById("password").value;
+// 	// let checkedBox = document.getElementById("saveUserCreds");
 
-	// if (checkedBox.check) {
-	// 	if (userDetails.length < 1) {
-	// 		userDetails.push([enteredUsername, enteredPassword]);
-	// 	}
-	// 	let [username, userpass] = userDetails.pop();
-	// 	document.getElementById("username").value = username;
-	// 	document.getElementById("password").value = userpass;
-	// }
+// 	// if (checkedBox.check) {
+// 	// 	if (userDetails.length < 1) {
+// 	// 		userDetails.push([enteredUsername, enteredPassword]);
+// 	// 	}
+// 	// 	let [username, userpass] = userDetails.pop();
+// 	// 	document.getElementById("username").value = username;
+// 	// 	document.getElementById("password").value = userpass;
+// 	// }
 
-	if (enteredPassword === "admin" && enteredUsername === "admin") {
-		window.location.href = "/ADMIN/AdminPanel/adminPanel.html";
-		// alert("Login Successful");
-	} else {
-		alert("This incident will be recorded");
-		console.log(
-			`Entered User Name: ${enteredUsername}, Entered User Password: ${enteredPassword}`
-		);
-	}
-}
+// 	if (enteredPassword === "admin" && enteredUsername === "admin") {
+// 		window.location.href = "/ADMIN/AdminPanel/adminPanel.html";
+// 		// alert("Login Successful");
+// 	} else {
+// 		alert("This incident will be recorded");
+// 		console.log(
+// 			`Entered User Name: ${enteredUsername}, Entered User Password: ${enteredPassword}`
+// 		);
+// 	}
+// }
 
 ///R code
 
