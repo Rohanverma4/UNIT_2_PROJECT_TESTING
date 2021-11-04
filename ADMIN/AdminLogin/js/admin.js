a04645a90daa01d96f60610d4b8b0a84f462b0de
I_am_rohan <88994067+Rohanverma4@users.noreply.github.com>
2021-11-04 18:59:31 +0530

    DOM part and admin panel security part

--- ADMIN/AdminLogin/js/admin.js
+++ ADMIN/AdminLogin/js/admin.js
@@ -25,3 +25,39 @@ function loginAdmin(event) {
 		);
 	}
 }
+
+///R code
+
+document.querySelector("form").addEventListener("submit", loginAdmin);
+let userDetails = JSON.parse(localStorage.getItem("Users")) || [];
+function loginAdmin(event) {
+	event.preventDefault();
+	let enteredUsername = document.getElementById("username").value;
+	let enteredPassword = document.getElementById("password").value;
+	// let checkedBox = document.getElementById("saveUserCreds");
+
+	// if (checkedBox.check) {
+	// 	if (userDetails.length < 1) {
+	// 		userDetails.push([enteredUsername, enteredPassword]);
+	// 	}
+	// 	let [username, userpass] = userDetails.pop();
+	// 	document.getElementById("username").value = username;
+	// 	document.getElementById("password").value = userpass;
+	// }
+	
+	if (enteredPassword === "admin" && enteredUsername === "admin") {
+		details = {"user":enteredUsername,"pass":enteredPassword}
+		localStorage.setItem("UserDetails",JSON.stringify(details));
+		window.location.href = "/ADMIN/AdminPanel/adminPanel.html";
+		// alert("Login Successful");
+	} else {
+		alert("This incident will be recorded");
+		console.log(
+			`Entered User Name: ${enteredUsername}, Entered User Password: ${enteredPassword}`
+		);
+	    var details = (
+			`Entered User Name: ${enteredUsername}, Entered User Password: ${enteredPassword}`
+		);
+        
+	}
+}
--- ADMIN/AdminPanel/js/connected.js
+++ ADMIN/AdminPanel/js/connected.js
@@ -89,20 +89,21 @@ var myFormTemplate = `
 					/>
 				</label>
 				<label for="productUrl"
-					>Product Image Url:
+					>Product Image:
 					<input type="text" id="productUrl" placeholder="Enter product Url" />
 				</label>
 
 				<label for="productAvaliability"
-					>Product Availability:
+					>Product Avaliability:
 					<select id="productAvaliability">
 						<option>-----</option>
 						<option value="yes">yes</option>
 						<option value="no">no</option>
 					</select>
 				</label>
-				<input type="submit" value="Submit Details" />
+				<input id="submitDetails" type="submit" value="Submit Details" />
 			</form>
+		
 `;
 var myUserDetails = `
 <h3>User Details</h3>
@@ -130,8 +131,6 @@ var myUserDetails = `
 var myAdminCredForm = `
 <div id="adminProfileView">
 				<div id="imgAndName">
-					<img src="https://via.placeholder.com/60" alt="profile image" />
-					<h2 id="adminName">Hi, Abc</h2>
 				</div>
 				<form id="adminProfile">
 					<label for="adminUserName">
@@ -183,12 +182,19 @@ var myDashboardHref = document.getElementById("dashBoard");
 myDashboardHref.addEventListener("click", () => {
 	document.getElementById("container").innerHTML = "";
 	document.getElementById("container").innerHTML = myPanelTemplate;
+
 });
+
+
 
 var myProductsHref = document.getElementById("productsBoard");
 myProductsHref.addEventListener("click", () => {
 	document.getElementById("container").innerHTML = "";
 	document.getElementById("container").innerHTML = myFormTemplate;
+	//console.log(1)
+	document.getElementById("submitDetails").addEventListener("click", function () {
+		addProductDetails();
+	})
 });
 
 var myUserDetailsHref = document.getElementById("allUsers");
@@ -201,9 +207,167 @@ var myAdminProfileHref = document.getElementById("adminHref");
 myAdminProfileHref.addEventListener("click", () => {
 	document.getElementById("container").innerHTML = "";
 	document.getElementById("container").innerHTML = myAdminCredForm;
+
+	//R CODE
+	var beforeUpdateDetails = document.getElementById("imgAndName");
+	var beforeProfile = JSON.parse(localStorage.getItem("adminProfile")) || [];
+	// GETTING IMAGE AND NAME
+	if (beforeProfile.length >= 1) {
+			var beforeName = (beforeProfile[beforeProfile.length - 1].nameOfAdmin)
+			var beforeImage = (beforeProfile[beforeProfile.length - 1].adminImageUrl)
+			// console.log(beforeImage,beforeName)
+			var img = document.createElement("img");
+			img.setAttribute("src", beforeImage);
+			img.setAttribute("alt", "profile image");
+
+			var name = document.createElement("h2");
+			name.setAttribute("id", "adminName");
+			name.textContent = `Hi, ${beforeName}`;
+
+			beforeUpdateDetails.append(img, name);
+	// //END
+	}
+	document.getElementById("submitAdminDetails").addEventListener("click", function () {
+		adminDetails();
+        location.href = "/ADMIN/AdminPanel/adminPanel.html";
+	})
 });
 
 var logOutHref = document.getElementById("logout");
 logOutHref.addEventListener("click", () => {
-	window.location.href = "/ADMIN/AdminLogin/admin.html";
+	window.localStorage.removeItem("UserDetails")
+	window.location.href="http://127.0.0.1:5500/ADMIN/AdminLogin/admin.html";
 });
+
+
+//  PUTTING PRODUCT DATA INTO DOM
+var productArr = JSON.parse(localStorage.getItem("productInfo")) || [];
+
+// FUNCTION FOR ADDING PRODUCT
+function addProductDetails() {
+	var product_name = document.getElementById("productName").value;
+	var product_price = document.getElementById("productPrice").value;
+	var product_details = document.getElementById("productDetails").value;
+	var product_description = document.getElementById("productDesc").value;
+	var product_image = document.getElementById("productUrl").value;
+	var product_avaliability = document.getElementById("productAvaliability").value;
+	// CREATING OBJECT FOR ARRAY..  
+	var productData = {
+		nameOfProduct: product_name,
+		price: product_price,
+		details: product_details,
+		description: product_description,
+		image_url: product_image,
+		avaliability: product_avaliability,
+	}
+	// ALERT FOR EMPTY ENTRY
+	var count = 0;
+	for (var input in productData) {
+		if (productData[input] == "") {
+			alert(`Please input the product ${input}`);
+			break;
+		} else {
+			count++;
+		}
+	}
+	// CHECKING ENTRY IS FULL OR NOT
+	if (count == Object.keys(productData).length) {
+		productArr.push(productData);
+		localStorage.setItem("productInfo", JSON.stringify(productArr));
+
+	}
+}
+
+
+
+//  PUTTING ADMIN DATA INTO DOM
+var adminInfoArr = JSON.parse(localStorage.getItem("adminProfile")) || [];
+
+// FUNCTION FOR INPUT ADMIN 
+function adminDetails() {
+	//console.log(1)
+	var adminUser_name = document.getElementById("adminUserName").value;
+	var admin_password = document.getElementById("adminPassword").value;
+	var confirm_password = document.getElementById("adminRetypePassword").value;
+	var admin_image = document.getElementById("adminImage").value;
+
+	if (admin_password != confirm_password) {
+		alert("Password and Confirm password is not matched");
+	} else {
+		// CREATING OBJECT FOR ARRAY..  
+		var adminData = {
+			nameOfAdmin: adminUser_name,
+			passwordOfAdmin: admin_password,
+			adminImageUrl: admin_image,
+		}
+		// ALERT FOR EMPTY ENTRY
+		var count = 0;
+		for (var input in adminData) {
+			if (adminData[input] == "") {
+				alert(`Please input the ${input}`);
+				break;
+			} else {
+				count++;
+			}
+		}
+		// CHECKING ENTRY IS FULL OR NOT
+		if (count == Object.keys(adminData).length) {
+			adminInfoArr.pop();
+			adminInfoArr.push(adminData);
+			localStorage.setItem("adminProfile", JSON.stringify(adminInfoArr));
+			// var headDiv = document.getElementById("imgAndName");
+
+			// var img = document.createElement("img");
+			// img.setAttribute("src", admin_image);
+			// img.setAttribute("alt","profile image");
+
+			// var name = document.createElement("h2");
+			// name.setAttribute("id", "adminName");
+			// name.textContent = `Hi, ${adminUser_name}`;
+
+			// headDiv.append(img,name);
+			alert ("Updated Successfully!!");
+		}
+
+	}
+}
+if (window.localStorage.getItem("UserDetails") == null) {
+	location.href = 'http://127.0.0.1:5500/ADMIN/AdminLogin/admin.html';
+}
+else {
+	var SavedUser = window.localStorage.getItem("UserDetails");
+	try {
+		SavedUser = JSON.parse(SavedUser);
+		if (SavedUser.user != 'admin' || SavedUser.pass != 'admin') {
+			location.href = 'http://127.0.0.1:5500/ADMIN/AdminLogin/admin.html';
+		}
+	} catch (error) {
+		location.href = 'http://127.0.0.1:5500/ADMIN/AdminLogin/admin.html';
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var headDiv = document.getElementById("imgAndName");
+
+			// var img = document.createElement("img");
+			// img.setAttribute("src", admin_image);
+			// img.setAttribute("alt","profile image");
+
+			// var name = document.createElement("h2");
+			// name.setAttribute("id", "adminName");
+			// name.textContent = `Hi, ${adminUser_name}`;
+
+			// headDiv.append(img,name);
