799e21364f87ec3aaa00db2e6c5c6e60c0bdd6b6
I_am_rohan <88994067+Rohanverma4@users.noreply.github.com>
2021-11-11 01:18:03 +0530

    LOGIN and ADMIN part DOM is completed..

--- LOGIN_PAGE/signup.js
+++ LOGIN_PAGE/signup.js
@@ -7,19 +7,22 @@
 //Checking Otp number
    document.querySelector(".btn").addEventListener("click",function(){
        checkOTP();
+     //  console.log(1)
    })
 
    // checkOTP function
+   var validated = true;
    function checkOTP(){
        var otp = document.querySelector(".otp").value;
        if (otp == "1234" && document.querySelector(".putNumber").value == number){
            window.location.href = "setPassword.html";
-       }  else {
+       }  else if (validated == true){
            var checkingOTP = document.querySelector(".notify");
            var invalidOTP = document.createElement("p");
            invalidOTP.setAttribute("class","checkMassage");
            invalidOTP.textContent="Invalid code. Try again with the correct code."
            //appending
            checkingOTP.append(invalidOTP);
+           validated = false;
        }
    }
---
+++ LOGIN_PAGE/wallet.js
@@ -0,0 +1,11 @@
+// Javascript code for wallet.html
+
+  // going for shopping
+  document.querySelector("button").addEventListener("click", function(){
+    addWallet();
+    console.log(1)
+})
+
+function addWallet(){
+    window.location.href = "http://127.0.0.1:5501/HOMEPAGE_CAROUSEL/first_page.html"
+}
